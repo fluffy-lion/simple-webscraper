d9edcb97497c3d555a4c00eafd2abf125899533f
fluffy-lion <fluffylionprojects@gmail.com>
2023-08-04 14:58:39 +0100

    collecting title, img url, release url and price

--- index.js
+++ index.js
@@ -8,21 +8,27 @@ const app = express()
 
 const url = 'https://www.discogs.com/sell/list?sort=listed%2Cdesc&limit=50&genre=Rock&ships_from=United+Kingdom&format=Vinyl&condition=Mint+%28M%29'
 // pass a url and get a response back
+const main = 'https://www.discogs.com'
 axios(url)
     .then(response => {
         const html = response.data
         const $ = cheerio.load(html)
-        const articles = []
+        const releases = []
         $('.shortcut_navigable', html).each(function() {
-            const title = $(this).find('a[class=item_description_title]').text()
+            const title = $(this).find(".item_description_title").text().trim()
+            const img = $(this).find(".marketplace_image").attr('data-src').trim()
+            const release =  main + $(this).find(".item_release_link").attr('href')
+            const price =$(this).find(".item_price .price").text().trim()
             //.hasClass('item_description_title')
             // const title = $(this).find('.item_description_title').attr('href')
-            articles.push({
+            releases.push({
                 title,
-                // url
+                img,
+                release,
+                price
             })
         })
-        console.log('item: ', articles)
+        console.log(releases)
     }).catch(err => console.log(err))
 
 app.listen(PORT, () => {
